a6319780cc83b198319b49fa38604476a7ae13eb
YarnikeyZ <83335375+YarnikeyZ@users.noreply.github.com>
2022-05-13 21:07:57 +0500

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,48 +1,61 @@
 ##Requirments: Python3, Figlet
 from time import sleep, strftime, localtime;
-from subprocess import check_output
+from subprocess import getoutput as gout
 from random import randint as rd
 
-clear = check_output('clear').decode('UTF-8')
+clear = gout('clear')
 
-def check_none(varz):
-	for var in range(0, 3):
-		if varz[var] == None:
-			varz[var] = 0
-	return varz
+def color(text, t_color, b_color):
+    return f'\033[38;5;{t_color}m\033[48;5;{b_color}m{text}\033[0;0m'
 
-def clock(ptime, style, t_color, b_color):
-	look = check_none([style, t_color, b_color])
-	look = f'\033[{look[0]};0;0m\033[38;5;{look[1]}m\033[48;5;{look[2]}m\n'
-	print(look, check_output(['figlet', strftime(ptime, localtime())]).decode('UTF-8')[1:][:-2], '\033[0;0m', end='')
+def big(text):
+    return f'{gout(f"figlet {text}")[1:]}\n'
+
+def get_time(str_time):
+    return strftime(str_time, localtime())
 
 try:
-	output = input('Type of color:').lower()
-	rainbow = [196, 208, 226, 82, 39, 21, 93, 196, 208]
-	green = [2, 22, 28, 34, 40, 46, 40, 34, 28, 22, 2, 22, 28]
-	while True:
-		if output == 'white' or output == 'w':
-				print(clear)
-				clock('[ %a . %b ] ', 1, 15, 0)
-				clock('[ %d . %m . %Y ] ', 1, 15, 0)
-				clock('%H : %M : %S     ', 1, 15, 0)
-				sleep(1)
-		elif output == 'rainbow' or output == 'r':
-			for rainbow_num in range(0, 7):
-				print(clear)
-				clock('[ %a . %b ] ', 1, rainbow[rainbow_num+2], 0)
-				clock('[ %d . %m . %Y ] ', 1, rainbow[rainbow_num+1], 0)
-				clock('%H : %M : %S     ', 1, rainbow[rainbow_num+0], 0)
-				sleep(0.1)
-		elif output == 'wtf':
-			print(clear)
-			WTF = rd(0,7)
-			clock('[ %a . %b ]  ', 1, 0, rainbow[WTF])
-			clock('[ %d . %m . %Y ]  ', 1, 0, rainbow[WTF])
-			clock('%H : %M : %S' , 1, 0, rainbow[WTF])
-			sleep(0.2)
-		else:
-			output = 'white'
+    type_of = input('Type of clock (w/r/wtf/c/p):').lower()
+    rainbow = [196, 208, 226, 82, 39, 21, 93, 196, 208]
+    types_times = ['%H', '%M', '%S', '%d', '%m', '%a', '%b', '%Y']
+    while True:
+        times = []
+        for tim in types_times:
+            times.append(get_time(tim))
+        if type_of == 'white' or type_of == 'w':
+            print(clear,
+            big(f'[ {times[5]} . {times[6]} ]'),
+            big(f'[ {times[3]} . {times[4]} . {times[7]} ]'),
+            big(f'{times[0]} : {times[1]} : {times[2]}'))
+            sleep(1)
+        elif type_of == 'rainbow' or type_of == 'r':
+            for rainbow_num in range(0, 7):
+                print(clear,
+                color(big(f'[ {times[5]} . {times[6]} ]'), rainbow[rainbow_num+2], 0),
+                color(big(f'[ {times[3]} . {times[4]} . {times[7]} ]'), rainbow[rainbow_num+1], 0),
+                color(big(f'{times[0]} : {times[1]} : {times[2]}'), rainbow[rainbow_num+0], 0))
+                sleep(0.1)
+        elif type_of == 'wtf':
+            WTF = rd(0,7)
+            print(clear,
+            color(big(f'[ {times[5]} . {times[6]} ]'), 0, rainbow[WTF]),
+            color(big(f'[ {times[3]} . {times[4]} . {times[7]} ]'), 0, rainbow[WTF]),
+            color(big(f'{times[0]} : {times[1]} : {times[2]}'), 0, rainbow[WTF]))
+            sleep(0.2)
+        elif type_of == 'compact' or type_of == 'c':
+            print(f"""{clear}\n\n\n\n\n
+                  Time: {times[0]}:{times[1]}:{times[2]}
+                  Date: {times[3]}.{times[4]}.{times[7]}
+                  Uptime: {gout('uptime').replace(' days,', 'd')[12:][:-42]}\n\n\n\n\n""")
+            sleep(1)
+        elif type_of == 'progress' or type_of == 'p':
+            print(f"""{clear}\n\n\n\n\n\n\n\n\n
+[Hour] [{times[0]}] [{color(''.ljust(int(times[0]), '#'), 9, 0)}{color(''.ljust(24-int(times[0]), '-'), 52, 0)}]
+[Min ] [{times[1]}] [{color(''.ljust(int(times[1]), '#'), 154, 0)}{color(''.ljust(60-int(times[1]), '-'), 100, 0)}]
+[Sec ] [{times[2]}] [{color(''.ljust(int(times[2]), '#'), 10, 0)}{color(''.ljust(60-int(times[2]), '-'), 34, 0)}]""")
+            sleep(1)
+        else:
+            exit()
 except KeyboardInterrupt:
-	print(clear)
-	exit()
+        print(clear)
+        exit()
